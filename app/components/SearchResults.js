c736617b0aa37533732f0a7e2ba800d2fc5d451c
Bartosz Reguła <barreg@o2.pl>
2024-08-26 21:44:10 +0200

    Added button_up to SearchBar

--- app/components/SearchResults.js
+++ app/components/SearchResults.js
@@ -1,12 +1,31 @@
+'use client';
+
 import Card from './Card';
 import styles from './SearchResults.module.css';
+import { useScrollTop } from '../hooks/useScrollTop.js';
+export default function SearchResults({ results, loading, page, totalPages, setPage }) {
 
-export default function SearchResults({ results }) {
+  const { isVisible } = useScrollTop({ loading, page, totalPages, setPage });
+
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div className={styles.container}>
       {results.map((show) => (
         <Card key={show.id} show={show} />
       ))}
+
+      {isVisible && (
+        <button className={styles.button_up} onClick={scrollToTop}>
+          Up
+        </button>
+      )}
     </div>
   );
 }
